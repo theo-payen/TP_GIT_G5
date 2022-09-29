aa521edc9654db74fd8b18ad0b3195152c4b5d77
mohamed wazane <92384537+mowazane@users.noreply.github.com>
2022-09-29 11:58:31 +0200

    fonction list nombre premier

--- list_nombre.py
+++ list_nombre.py
@@ -1,10 +1,18 @@
 #script pour le TP GIT 
 #il doit afficher la liste des nombres premiers entre 1 et 100 000
 #acteurs @mohamed, @theo, @mukil 
-min = 1
-max = 10000
 
-while min < max :
-    min = min + 1
-    print (min)
+def nombrepremier(n) :
+    if n <= 1 :
+        return False
+    for i in range(2, n) :
+        if (n % i == 0) :
+            return False
+    return True
 
+
+def compteurNb(min, max):
+    while min < max :
+        min = min + 1
+        if nombrepremier(min) == True:
+            print (min)
