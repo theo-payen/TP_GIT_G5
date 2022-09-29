9f838c7bec48ade3a66e876a309cbbc5dd6e4d12
Theo PAYEN <tt.payen@gmail.com>
2022-09-29 11:28:35 +0200

    modification du script list_nombre en fonction

--- list_nombre.py
+++ list_nombre.py
@@ -1,10 +1,9 @@
 #script pour le TP GIT 
-#il doit afficher la liste des nombres premiers entre 1 et 100 000
+#il doit afficher la liste des nombres premiers 
 #acteurs @mohamed, @theo, @mukil 
-min = 1
-max = 10000
 
-while min < max :
-    min = min + 1
-    print (min)
 
+def compteurNb(min, max):
+    while min < max :
+        min = min + 1
+        print (min)
