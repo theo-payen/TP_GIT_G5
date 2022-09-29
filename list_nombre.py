dba9f74441f89918b7b560bc55482e1c8a8f06f1
mohamed wazane <92384537+mowazane@users.noreply.github.com>
2022-09-29 11:02:44 +0200

    script doit afficher la liste des nombres premiers entre 1 et 100 000

---
+++ list_nombre.py
@@ -0,0 +1,10 @@
+#script pour le TP GIT 
+#il doit afficher la liste des nombres premiers entre 1 et 100 000
+#acteurs @mohamed, @theo, @mukil 
+min = 1
+max = 10000
+
+while min < max :
+    min = min + 1
+    print (min)
+
