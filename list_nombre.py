056ac678b4cacf553a567faf17564f8add213339
Theo PAYEN <tt.payen@gmail.com>
2022-09-29 12:15:54 +0200

    resolve conflict

--- list_nombre.py
+++ list_nombre.py
@@ -3,7 +3,18 @@
 #acteurs @mohamed, @theo, @mukil 
 
 
+def nombrepremier(n) :
+    if n <= 1 :
+        return False
+    for i in range(2, n) :
+        if (n % i == 0) :
+            return False
+    return True
+
+
 def compteurNb(min, max):
     while min < max :
         min = min + 1
-        print (min)
+        if nombrepremier(min) == True:
+            print (min)
+
